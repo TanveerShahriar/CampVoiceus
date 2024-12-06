e1ea819b7f75e880458e16d56b429d78716d5d22
TanveerShahriar <tanveershahriara@gmail.com>
2024-12-06 17:27:22 +0600

    backend model

--- backend/controllers/user.controller.mjs
+++ backend/controllers/user.controller.mjs
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import User from '../models/user.model.mjs';
+import { User } from '../models/index.mjs';
 
 const generateToken = (user) => {
     return jwt.sign(
