8c811f08fd660378c5a25e970ce9b55ac886bb16
TanveerShahriar <tanveershahriara@gmail.com>
2024-12-16 21:41:16 +0600

    route protected

--- backend/routes/user.routes.mjs
+++ backend/routes/user.routes.mjs
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/index.mjs';
+import { requireAuth } from '../middleware/requireAuth.mjs';
 
 const router = Router();
 
 router.get('/', UserController.welcome);
 router.post('/register', UserController.registerUser);
 router.post('/login', UserController.loginUser);
+
+router.use(requireAuth);
+
 router.post('/getuserbyid', UserController.getUserById);
 
 export default router;
