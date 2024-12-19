8ecad00bbcb5e15e4509770c4b84f3985e30169b
Mehedi Hasan <mehedihtanvir@gmail.com>
2024-12-19 18:37:31 +0600

    updating cors policy

--- backend/app.mjs
+++ backend/app.mjs
@@ -6,7 +6,23 @@ import routes from './routes/index.mjs';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+// Configure CORS options
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      // Allow requests with no origin (e.g., mobile apps, Postman) or a valid origin
+      callback(null, true);
+    } else {
+      callback(new Error(`Origin ${origin} not allowed by CORS`));
+    }
+  },
+};
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
 
 connectDB();
 
