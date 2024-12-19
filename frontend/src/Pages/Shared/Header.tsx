a92003c068e08c4dee96bc7c07b2f64b02bf9a6c
Mehedi Hasan <mehedihtanvir@gmail.com>
2024-12-19 18:26:31 +0600

    added margin in navbar bottom

--- frontend/src/Pages/Shared/Header.tsx
+++ frontend/src/Pages/Shared/Header.tsx
@@ -20,7 +20,7 @@ export default function Header() {
   };
 
   return (
-    <nav className="bg-indigo-600 shadow-lg">
+    <nav className="bg-indigo-600 shadow-lg mb-2">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
